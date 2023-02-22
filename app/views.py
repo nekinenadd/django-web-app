500c1030f43392c42c409c193b839b04af04f5d5
Nenad Djurdjevic <nenad.djurdjevic2002@gmail.com>
2023-02-22 17:20:14 +0100

    integrating comments and sorting out models

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from app.models import Post
+from app.models import Post,Tag
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -11,5 +12,20 @@ def index(request):
 
 def post_page(request,slug):
     post = Post.objects.get(slug=slug)
-    context = {"post":post}
+    form = CommentForm()
+
+    if request.POST:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid:
+            comment = comment_form.save(commit=False)
+
+
+
+    if post.view_count is None:
+        post.view_count = 1
+    else:
+        post.view_count = post.view_count + 1
+    post.save()
+
+    context = {"post":post,'form': form }
     return render(request, 'app/post.html',context)
